15876d936642b1c87b7ce18619b48cd8ac623305
JakeD <jakee2000jake@gmail.com>
2023-08-26 09:30:13 -0500

    Add project files.

---
+++ Services/Spotify/SpotifyService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyDownloader.Services.Spotify
+{
+    public class SpotifyService
+    {
+
+    }
+}
